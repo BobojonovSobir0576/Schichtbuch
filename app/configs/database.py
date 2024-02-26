cfa9298fee8ce6069921622edeae83b33fe34cc9
BobojonovSobir0576 <dweb0987654321@gmail.com>
2024-02-26 19:11:16 +0500

    msql seetings

--- app/configs/database.py
+++ app/configs/database.py
@@ -5,6 +5,12 @@ from sqlalchemy.orm import sessionmaker
 # For SQLite, you just need the path to the database file. Use ":memory:" for an in-memory database.
 SQLITE_DATABASE_URL = "sqlite:///./test.db"
 
+# MYSQL_DATABASE_URL = f"mysql+pymysql://{settings.db_user}:{settings.db_password}@{settings.db_host}:{settings.db_port}/{settings.db_name}"
+#
+# engine = create_engine(
+#     MYSQL_DATABASE_URL, echo=True, pool_recycle=3600
+# )
+
 engine = create_engine(
     SQLITE_DATABASE_URL, connect_args={"check_same_thread": False}, echo=True
 )
